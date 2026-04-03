ae5bbe2671fd90c0b723f246dae3f1fbda8d9103
Nyarlotech <34801423+nyarlotech@users.noreply.github.com>
2026-04-03 17:35:14 -0400

    More work on weapons

--- source/scripts/workers.js
+++ source/scripts/workers.js
@@ -80,51 +80,33 @@ coreAttributes.forEach(attribute => {
     on(`change:${attribute}`, function (eventInfo) {
         const attScore = Math.floor((parseInt(eventInfo.newValue) || 0));
         let modScore;
-        switch(attScore) {
-            case 3:
-                modScore = -4;
-                break;
-            case 4:
-            case 5:
-                modScore = -3;
-                break;
-            case 6:
-            case 7:
-                modScore = -2;
-                break;
-            case 8:
-            case 9:
-                modScore = -1;
-                break;
-            case 10:
-            case 11:
-                modScore = 0;
-                break;
-            case 12:
-            case 13:
-                modScore = 1;
-                break;
-            case 14:
-            case 15:
-                modScore = 2;
-                break;
-            case 16:
-            case 17:
-                modScore = 3;
-                break;
-            case 18:
-            case 19:
-                modScore = 4;
-                break;
-            default:
-                modScore = 5;
-                break;
-        }
+        modScore = attScore < 2 ? -5 :
+                   attScore < 4 ? -4 :
+                   attScore < 6 ? -3 :
+                   attScore < 8 ? -2 :
+                   attScore < 10 ? -1 :
+                   attScore < 12 ? 0 :
+                   attScore < 14 ? 1 :
+                   attScore < 16 ? 2 :
+                   attScore < 18 ? 3 :
+                   attScore < 20 ? 4 :
+                   attScore < 22 ? 5 :
+                   attScore < 24 ? 6 :
+                   attScore < 26 ? 7 :
+                   attScore < 28 ? 8 :
+                   attScore < 30 ? 9 : 10;
         log("Modifier", modScore, derivedStatsColor)
         const attrsToSet = {};
         attrsToSet[`${attribute}_modifier`] = modScore;
         setAttrs(attrsToSet);
     });
+});
+
+// Heroic Proficiency Bonus Calculation
+on('change:level', function(eventInfo) {
+    const level = parseInt(eventInfo.newValue) || 0;
+    let hpb = level < 5 ? 2 : level < 9 ? 3 : level < 13 ? 4 : level < 17 ? 5 : 6;
+    setAttrs({ "heroic_proficiency_bonus": hpb });
 });
 
 // Update Corruption
@@ -162,44 +144,47 @@ on('change:condition', function(eventInfo) {
 });
 
 // Watch for Fort Save Changes
-on('change:level_or_armor change:class_bonus change:constitution_modifier change:fort_misc_save_mod', function() {
-    getAttrs(['level_or_armor', 'class_bonus', 'constitution_modifier', 'FORT_misc_save_mod'], function(v) {
-        console.log("v ", v);
+on('change:level_or_armor change:fort_class_bonus change:constitution_modifier change:fort_misc_save_mod change:condition_selected', function() {
+    getAttrs(['level_or_armor', 'fort_class_bonus', 'constitution_modifier', 'fort_misc_save_mod', 'condition_selected'], function(v) {
+        console.log("v for Fort Save ", v);
+        const condition = parseInt(v.condition_selected.split(' ')[0]||0);
         const levelOrArmor = parseInt(v.level_or_armor) || 0;
-        const classBonus = parseInt(v.class_bonus) || 0;
+        const classBonus = parseInt(v.fort_class_bonus) || 0;
         const constitutionModifier = parseInt(v.constitution_modifier) || 0;
-        const fortMiscSaveMod = parseInt(v.FORT_misc_save_mod) || 0;
-        log("Fort Save Calculation", `10 + ${levelOrArmor} (Level/Armor) + ${classBonus} (Class Bonus) + ${constitutionModifier} (Constitution Modifier) + ${fortMiscSaveMod} (Misc Mod)`, r20color);
-        const fortSaveTotal = 10+levelOrArmor + classBonus + constitutionModifier + fortMiscSaveMod;
+        const fortMiscSaveMod = parseInt(v.fort_misc_save_mod) || 0;
+        log("Fort Save Calculation", `10 + ${levelOrArmor} (Level/Armor) + ${classBonus} (Class Bonus) + ${constitutionModifier} (Constitution Modifier) + ${fortMiscSaveMod} (Misc Mod) + ${condition} (Condition)`, r20color);
+        const fortSaveTotal = 10+levelOrArmor + classBonus + constitutionModifier + fortMiscSaveMod + condition;
         const setattrs = {};
-        setattrs['FORT_total'] = fortSaveTotal;
+        setattrs['fort_total'] = fortSaveTotal;
         setattrs['stamina_fortDef'] = fortSaveTotal;
         setAttrs(setattrs);
     });
 });
 
 // Watch for Ref Save Changes
-on('change:level_or_armor change:class_bonus change:dexterity_modifier change:ref_misc_save_mod', function() {
-    getAttrs(['level_or_armor', 'class_bonus', 'dexterity_modifier', 'REF_misc_save_mod'], function(v) {
+on('change:level_or_armor change:ref_class_bonus change:dexterity_modifier change:ref_misc_save_mod change:condition_selected', function() {
+    getAttrs(['level_or_armor', 'ref_class_bonus', 'dexterity_modifier', 'ref_misc_save_mod', 'condition_selected'], function(v) {
+        const condition = parseInt(v.condition_selected.split(' ')[0]||0);
         const levelOrArmor = parseInt(v.level_or_armor) || 0;
-        const classBonus = parseInt(v.class_bonus) || 0;
+        const classBonus = parseInt(v.ref_class_bonus) || 0;
         const dexterityModifier = parseInt(v.dexterity_modifier) || 0;
-        const refMiscSaveMod = parseInt(v.REF_misc_save_mod) || 0;
+        const refMiscSaveMod = parseInt(v.ref_misc_save_mod) || 0;
 
-        const refSaveTotal = 10+levelOrArmor + classBonus + dexterityModifier + refMiscSaveMod;
-        setAttrs({ REF_total: refSaveTotal });
+        const refSaveTotal = 10+levelOrArmor + classBonus + dexterityModifier + refMiscSaveMod + condition;
+        setAttrs({ ref_total: refSaveTotal });
     });
 });
 
 // Watch for Will Save Changes
-on('change:level_or_armor change:class_bonus change:wisdom_modifier change:will_misc_save_mod', function() {
-    getAttrs(['level_or_armor', 'class_bonus', 'wisdom_modifier', 'WILL_misc_save_mod'], function(v) {
+on('change:level_or_armor change:will_class_bonus change:wisdom_modifier change:will_misc_save_mod change:condition_selected', function() {
+    getAttrs(['level_or_armor', 'will_class_bonus', 'wisdom_modifier', 'will_misc_save_mod', 'condition_selected'], function(v) {
+        const condition = parseInt(v.condition_selected.split(' ')[0]||0);
         const levelOrArmor = parseInt(v.level_or_armor) || 0;
-        const classBonus = parseInt(v.class_bonus) || 0;
+        const classBonus = parseInt(v.will_class_bonus) || 0;
         const wisdomModifier = parseInt(v.wisdom_modifier) || 0;
-        const willMiscSaveMod = parseInt(v.WILL_misc_save_mod) || 0;
+        const willMiscSaveMod = parseInt(v.will_misc_save_mod) || 0;
 
-        const willSaveTotal = 10+levelOrArmor + classBonus + wisdomModifier + willMiscSaveMod;
-        setAttrs({ WILL_total: willSaveTotal });
+        const willSaveTotal = 10+levelOrArmor + classBonus + wisdomModifier + willMiscSaveMod + condition;
+        setAttrs({ will_total: willSaveTotal });
     });
 });
