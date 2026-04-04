58b57f077b421e99ba7d03a5b62399f80178855e
Nyarlotech <34801423+nyarlotech@users.noreply.github.com>
2026-04-04 08:44:38 -0400

    More work on Weapons

--- source/scripts/weapons.js
+++ source/scripts/weapons.js
@@ -1,6 +1,20 @@
 // Sheet Workers for Weapons
+//--------------------------
+//rereating_weapons_weapon = Weapon Name
+//repeating_weapons_weapon-atk = Total ATK (Heroic Proficiency Bonus + Attack Bonus from Str/Dex + Misc Modifiers)
+//repeating_weapons_weapon-atk-bonus = ATK Attack Bonus (e.g. STR/STRx2/DEX)
+//  - str
+//  - strx2
+//  - dex
+//repeating_weapons_weapon-misc-mod = ATK Miscellaneous Attack Modifier (from magic items, etc.)
+//repeating_weapons_weapon-type = Weapon Type (Melee, Ranged)
+//repeating_weapons_weapon-damage = Weapon Damage (e.g. 1d6)
+//repeating_weapons_weapon-crit = Weapon Critical (e.g. 20 or lowest natural roll on a d20 for a critical hit)
+//repeating_weapons_attack_formula = Formula for calculating attack (e.g. "HPB + STR/DEX + Misc Mod")
 
 on('change:repeating_weapons:weapon change:repeating_weapons:weapon-atk-bonus change:repeating_weapons:weapon-misc-mod change:heroic-proficiency-bonus', function(eventInfo) {
+    log('Repeating Weapons Change Triggered by: ' + eventInfo.sourceAttribute, r20color);
+    console.log(eventInfo);
     const fields = [];
     if (eventInfo.sourceAttribute === 'heroic-proficiency-bonus') {
         // Heroic Proficiency Trigger - must update ALL weapons
@@ -12,14 +26,21 @@ on('change:repeating_weapons:weapon change:repeating_weapons:weapon-atk-bonus ch
     }
     else {
         // Grab only the values for the current weapon
-        fields.push("heroic-proficiency-bonus","repeating_weapons_weapon-atk-bonus","repeating_weapons_weapon-misc-mod");
+        fields.push("heroic_proficiency_bonus","repeating_weapons_weapon-atk-bonus","repeating_weapons_weapon-misc-mod", "strength_modifier", "dexterity_modifier", "repeating_weapons_weapon-type");
+        console.log('Fields to get: ', fields);
         getAttrs(fields, function(values) {
-            // do the work
-            const hpb = parseInt(values['heroic-proficiency-bonus']) || 0;
-            const atkBonus = parseInt(values['repeating_weapons_weapon-atk-bonus']) || 0;
+            console.log(values);
+            const setattrs = {};
+            const hpb = parseInt(values['heroic_proficiency_bonus']) || 0;
             const miscMod = parseInt(values['repeating_weapons_weapon-misc-mod']) || 0;
+            const attributeBonus = values['repeating_weapons_weapon-atk-bonus'];
+            const atkBonus = attributeBonus === 'str' ? parseInt(values['strength_modifier']) || 0 :
+                            attributeBonus === 'strx2' ? 2 * (parseInt(values['strength_modifier']) || 0) :
+                            attributeBonus === 'dex' ? parseInt(values['dexterity_modifier']) || 0 : 0;
 
             const totalAtk = hpb + atkBonus + miscMod;
+            setattrs['repeating_weapons_weapon-atk'] = totalAtk;
+            setAttrs(setattrs);
         });
     }
 });
