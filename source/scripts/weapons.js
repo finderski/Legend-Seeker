94bc53475fc74d83b7a8924b02bbb0f4f5e9a06d
Nyarlotech <34801423+nyarlotech@users.noreply.github.com>
2026-04-05 00:46:19 -0400

    Weapon Layout Complete.

--- source/scripts/weapons.js
+++ source/scripts/weapons.js
@@ -12,40 +12,12 @@
 //repeating_weapons_weapon-crit = Weapon Critical (e.g. 20 or lowest natural roll on a d20 for a critical hit)
 //repeating_weapons_attack_formula = Formula for calculating attack (e.g. "HPB + STR/DEX + Misc Mod")
 
-on('change:repeating_weapons:weapon change:repeating_weapons:weapon-atk-bonus change:repeating_weapons:weapon-misc-mod change:heroic-proficiency-bonus', function(eventInfo) {
-    log('Repeating Weapons Change Triggered by: ' + eventInfo.sourceAttribute, r20color);
-    console.log(eventInfo);
-    const fields = [];
-    if (eventInfo.sourceAttribute === 'heroic-proficiency-bonus') {
-        // Heroic Proficiency Trigger - must update ALL weapons
-        getSectionIDs('repeating_weapons', function(ids) {
-            ids.forEach(function(id) {
-                calculateWeaponAttack(id);
-            });
-        });
-    }
-    else {
-        // Grab only the values for the current weapon
-        fields.push("heroic_proficiency_bonus","repeating_weapons_weapon-atk-bonus","repeating_weapons_weapon-misc-mod", "strength_modifier", "dexterity_modifier", "repeating_weapons_weapon-type");
-        console.log('Fields to get: ', fields);
-        getAttrs(fields, function(values) {
-            console.log(values);
-            const setattrs = {};
-            const hpb = parseInt(values['heroic_proficiency_bonus']) || 0;
-            const miscMod = parseInt(values['repeating_weapons_weapon-misc-mod']) || 0;
-            const attributeBonus = values['repeating_weapons_weapon-atk-bonus'];
-            const atkBonus = attributeBonus === 'str' ? parseInt(values['strength_modifier']) || 0 :
-                            attributeBonus === 'strx2' ? 2 * (parseInt(values['strength_modifier']) || 0) :
-                            attributeBonus === 'dex' ? parseInt(values['dexterity_modifier']) || 0 : 0;
-
-            const totalAtk = hpb + atkBonus + miscMod;
-            setattrs['repeating_weapons_weapon-atk'] = totalAtk;
-            setAttrs(setattrs);
-        });
-    }
-});
-
 // Function to update Half-Level and Heroic Proficiency when they change after the sheet has been opened.
+
+// listOfAttributes - contains all Attribute Modifier Fields (e.g. strength_modifier, dexterity_modifier, etc.)
+const damageFields = ["repeating_weapons_weapon-half-level", "repeating_weapons_weapon_damage_dice", "repeating_weapons_weapon-damage-attribute-mod", "repeating_weapons_weapon-damage-misc-mod"];
+const atkFields = ["repeating_weapons_weapon-heroic-proficiency", "repeating_weapons_weapon-atk-bonus", "repeating_weapons_weapon-misc-mod"];
+
 function updateWeapons(derivedStat) {
     const fields = [
         "half-level",
@@ -76,3 +48,68 @@ function updateWeapons(derivedStat) {
         });
     });
 }
+
+// Watch for a new weapon to be added and set the half-level and heroic proficiency bonus for that weapon.
+on('change:repeating_weapons:weapon change:repeating_weapons:weapon-type', function(eventInfo) {
+    log('Repeating Weapons Change Triggered by: ' + eventInfo.sourceAttribute, r20color);
+    getAttrs(['half-level', 'heroic_proficiency_bonus'], function(values) {
+        const halfLevel = values['half-level'] || 0;
+        const heroicProficiencyBonus = values['heroic_proficiency_bonus'] || 0;
+        const setattrs = {};
+        setattrs[`repeating_weapons_${eventInfo.sourceAttribute.split('_')[2]}_weapon-half-level`] = halfLevel;
+        setattrs[`repeating_weapons_${eventInfo.sourceAttribute.split('_')[2]}_weapon-heroic-proficiency`] = heroicProficiencyBonus;
+        setAttrs(setattrs);
+    });
+});
+
+// Watch for changes to Damage Fields to Update Total Damage
+on('change:repeating_weapons:weapon change:repeating_weapons:weapon-type change:repeating_weapons_weapon-damage change:repeating_weapons_weapon-misc-mod change:heroic-proficiency-bonus', function(eventInfo) {
+    log('Repeating Weapons Change Triggered by: ' + eventInfo.sourceAttribute, r20color);
+    console.log(eventInfo);
+    const fields = [];
+});
+
+// Watch for Changes to ATK Fields to Update Total ATK
+on('change:repeating_weapons:weapon change:repeating_weapons:weapon-atk-bonus change:repeating_weapons:weapon-misc-mod change:heroic-proficiency-bonus', function(eventInfo) {
+    log('Repeating Weapons Change Triggered by: ' + eventInfo.sourceAttribute, r20color);
+    console.log(eventInfo);
+    const fields = [];
+    if (eventInfo.sourceAttribute === 'heroic-proficiency-bonus') {
+        // Heroic Proficiency Trigger - must update ALL weapons
+        getSectionIDs('repeating_weapons', function(ids) {
+            ids.forEach(function(id) {
+                calculateWeaponAttack(id);
+            });
+        });
+    }
+    else {
+        // Grab only the values for the current weapon
+        fields.push(
+            // Damage Formula Fields
+            "weapon-half-level",
+            // ATK Formula Fields
+            "repeating_weapons_weapon-atk-bonus",
+            "repeating_weapons_weapon-misc-mod",
+            //Both Formula Fields
+            "strength_modifier",
+            "dexterity_modifier",
+            "repeating_weapons_weapon-type"
+        );
+        console.log('Fields to get: ', fields);
+        getAttrs(fields, function(values) {
+            console.log(values);
+            const setattrs = {};
+            const hpb = parseInt(values['heroic_proficiency_bonus']) || 0;
+            const miscMod = parseInt(values['repeating_weapons_weapon-misc-mod']) || 0;
+            const attributeBonus = values['repeating_weapons_weapon-atk-bonus'];
+            const atkBonus = attributeBonus === 'str' ? parseInt(values['strength_modifier']) || 0 :
+                            attributeBonus === 'strx2' ? 2 * (parseInt(values['strength_modifier']) || 0) :
+                            attributeBonus === 'dex' ? parseInt(values['dexterity_modifier']) || 0 : 0;
+
+            const totalAtk = hpb + atkBonus + miscMod;
+            setattrs['repeating_weapons_weapon-atk'] = totalAtk;
+            setAttrs(setattrs);
+        });
+    }
+});
+
