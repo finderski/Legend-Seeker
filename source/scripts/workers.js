6f2e78cc2f6f9dbf07f19074fbcd7a935d87d42e
Nyarlotech <34801423+nyarlotech@users.noreply.github.com>
2026-04-06 08:53:13 -0400

    Fixed Corruption and started RollTemplate

--- source/scripts/workers.js
+++ source/scripts/workers.js
@@ -100,19 +100,39 @@ on('change:level', function(eventInfo) {
     setAttrs({ "heroic_proficiency_bonus": hpb });
 });
 
+
 // Update Corruption
-on('change:wisdom', function(eventInfo) {
-    const wisdomScore = Math.floor((parseInt(eventInfo.newValue) || 0));
-    const setattrs = {};
-    for(let i=1; i <= 24; i++) {
-        // while i is the less than 24, hide the lock inputs
-        if (i <= wisdomScore) {
-            setattrs[`corruption_lock_${i}`] = 'show';
-        } else {
-            setattrs[`corruption_lock_${i}`] = 'hide';
+on('change:wisdom change:corruption_1 change:corruption_2 change:corruption_3 change:corruption_4 change:corruption_5 change:corruption_6 change:corruption_7 change:corruption_8 change:corruption_9 change:corruption_10 change:corruption_11 change:corruption_12 change:corruption_13 change:corruption_14 change:corruption_15 change:corruption_16 change:corruption_17 change:corruption_18 change:corruption_19 change:corruption_20 change:corruption_21 change:corruption_22 change:corruption_23 change:corruption_24', function(eventInfo) {
+    //log('Corruption Watch Detected Change', JSON.stringify(eventInfo), r20color);
+    if (eventInfo.sourceAttribute === 'wisdom') {
+        // update the number of corruption slots
+        const wisdomScore = Math.floor((parseInt(eventInfo.newValue) || 0));
+        const setattrs = {};
+        for(let i=1; i <= 24; i++) {
+            // while i is the less than 24, hide the lock inputs
+            if (i <= wisdomScore) {
+                setattrs[`corruption_lock_${i}`] = 'show';
+            } else {
+                setattrs[`corruption_lock_${i}`] = 'hide';
+            }
         }
+        setAttrs(setattrs);
     }
-    setAttrs(setattrs);
+    else {
+        // update the shading of the corruption slots        const corruptionValue = parseInt(eventInfo.newValue) || 0;
+        const setattrs = {};
+        const corruptionValue = parseInt(eventInfo.sourceAttribute.split('_')[1]) || 0; // get the number from corruption_1, corruption_2, etc.
+        //log("Corruption Value", corruptionValue, r20color);
+        for(let i=1; i <= 24; i++) {
+            if (i <= corruptionValue) {
+                setattrs[`corruption_${i}`] = 'on';
+            } else {
+                setattrs[`corruption_${i}`] = 0;
+            }
+        }
+        setAttrs(setattrs,{silent:true});
+    }
+    
 });
 
 // Watch for changes for Stamina Damage Threshold
