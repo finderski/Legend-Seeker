40b93dc10d1262302814216ec9d38a11ebff03ea
Nyarlotech <34801423+nyarlotech@users.noreply.github.com>
2026-04-22 09:03:33 -0400

    Damage Reduction on First Page

--- source/scripts/onOpen.js
+++ source/scripts/onOpen.js
@@ -10,12 +10,53 @@ function damageThreshold () {
     });
 };
 
+function compareVersions(a, b) {
+    const aParts = a.split('.').map(Number);
+    const bParts = b.split('.').map(Number);
+    const len = Math.max(aParts.length, bParts.length);
+
+    for (let i = 0; i < len; i++) {
+        const aVal = aParts[i] || 0;
+        const bVal = bParts[i] || 0;
+
+        if (aVal < bVal) return -1;
+        if (aVal > bVal) return 1;
+    }
+
+    return 0;
+}
+
+const sheetVersionFields = ['sheetversion','shield_dr','armor_dr'];
+
+on('sheet:opened', function() {
+    getAttrs(sheetVersionFields, function(values) {
+        const currentVersion = '0.1.1';
+        const sheetVersion = values.sheetversion || '0.0.0';
+        const shield_dr = parseInt(values.shield_dr) || 0;
+        const armor_dr = parseInt(values.armor_dr) || 0;
+        log("Sheet Version", sheetVersion, r20color);
+        // Check if sheetversion is less than 0.1.1, if so, update the weapon attributes. This ensures that we don't overwrite any existing values for current users, but new users will have the correct attributes from the start.
+        if (compareVersions(sheetVersion, '0.1.1') < 0) {
+            log('Updates for version 0.1.1', "update total_dr", r20color);
+            const setattrs = {};
+            setattrs['sheetversion'] = currentVersion;
+            setattrs['total_dr'] = shield_dr + armor_dr;
+            setAttrs(setattrs);
+        }
+        else {
+            log('Sheet version is up to date', "No updates needed", r20color);
+        }
+    });
+});
+
 // Need to make sure that every weapon has the half level and heroic proficiency attributes, even if they aren't used, so that the calculations for those attributes work properly.
 on('sheet:opened', function() {
     log('Sheet Opened',"Updating weapon attributes to ensure calculations work", r20color);
     damageThreshold();
     getSectionIDs('repeating_weapons', function(ids) {
         getAttrs(['heroic_proficiency_bonus','half-level'], function(values) {
+            
+            // Update Weapons stuff
             ids.forEach(function(id) {
                 const attrsToSet = {};
                 attrsToSet[`repeating_weapons_${id}_weapon-half-level`] = values['half-level'] || 0;
